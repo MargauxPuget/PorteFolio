261e2d2c2be2ef2043635f79213c3d1ecb44dff0
MargauxPuget <margaux.puget@gmail.com>
2022-10-06 14:41:55 +0200

    accueil version mobile DONE

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { getLocaleMonthNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
 
   constructor() { }
 
